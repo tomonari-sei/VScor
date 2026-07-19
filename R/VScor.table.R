09712a4d8f4599406c3ae2929fb1f8484ce58cbf
Tomonari SEI <sei@mist.i.u-tokyo.ac.jp>
2026-07-20 00:12:17 +0900

    Remove duplicated codes in VScor.table

--- R/VScor.table.R
+++ R/VScor.table.R
@@ -9,25 +9,24 @@
 #' VScor.table(N)
 
 VScor.table = function(x, ...){
+  n = sum(x)
+  phat = x / n
   if(nrow(x) == 2 && ncol(x) == 2){ # 2 by 2 table
-    n = sum(x)
-    phat = x / n
     a = phat[1,1]; b = phat[1,2]; c = phat[2,1]; d = phat[2,2]
     delta = 2 * (sqrt(a * d) - sqrt(b * c))
     delta = max(-1, min(1, delta))  # avoiding numerical issues
     s = asin(delta)  # signed Riemannian distance (variance-stabilizing transformation)
     RD = abs(s)      # Riemannian distance
-    z = 2 * atanh(sin(RD / 2))
-    VScor = tanh(z)  # Variance-stabilizable correlation
-    VScor_squared = VScor^2
-    return(list(signed = s, distance = RD, distance_se = 1/sqrt(n), VScor = VScor, VScor_squared = VScor_squared))
+  }else{
+    lambda = min(max(0, svd(sqrt(phat), 1, 1)$d[1]), 1)
+    RD = asin(sqrt(1-lambda^2)) * 2  # Riemannian distance
   }
-  n = sum(x)
-  phat = x / n
-  lambda = min(max(0, svd(sqrt(phat), 1, 1)$d[1]), 1)
-  RD = asin(sqrt(1-lambda^2)) * 2  # Riemannian distance
-  z = 2 * atanh(sqrt(1-lambda^2))
+  z = 2 * atanh(sin(RD / 2))
   VScor = tanh(z)  # Variance-stabilizable correlation
   VScor_squared = VScor^2
-  list(distance = RD, distance_se = 1/sqrt(n), VScor = VScor, VScor_squared = VScor_squared)
+  if(nrow(x) == 2 && ncol(x) == 2){
+    return(list(signed = s, distance = RD, distance_se = 1/sqrt(n), VScor = VScor, VScor_squared = VScor_squared))
+  }else{
+    return(list(distance = RD, distance_se = 1/sqrt(n), VScor = VScor, VScor_squared = VScor_squared))
+  }
 }
